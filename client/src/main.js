2b34176e5f5ea24a82c07d582afbeda51cd36569
JGluhov <jgluhov@gmail.com>
2016-02-06 01:22:52 +0300

    add cube

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,46 @@
 /**
  * Created by jgluhov on 05/02/16.
  */
-let k = () => {
-	console.log('Hello')
-};
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/main.styl';
+import {THREE} from 'three.js';
+
+(function() {
+
+	function main() {
+
+		var scene = new THREE.Scene();
+
+		var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 1, 1000 );
+		camera.position.z = 500;
+
+
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor( 0x434343 );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( renderer.domElement );
+
+		var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+		var material = new THREE.MeshBasicMaterial( { color: 0x838383 } );
+		var cube = new THREE.Mesh( geometry, material );
+		cube.rotateY(100);
+		cube.rotateZ(90);
+		cube.rotateX(150);
+
+
+		scene.add( cube );
+
+
+		var render = function () {
+			requestAnimationFrame( render );
+			renderer.render(scene, camera);
+		};
+
+		render();
+
+	}
+
+	document.addEventListener('DOMContentLoaded', main)
+
+})();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,54 @@
 /**
  * Created by jgluhov on 05/02/16.
  */
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	context: __dirname + '/src',
-	entry: './main',
+	entry:  {
+		main: [
+			'webpack-dev-server/client?http://localhost:8080',
+			'./main.js'
+		]
+	},
 	output: {
 		path: __dirname + '/public',
-		publicPath: '/public',
-		filename: 'bundle.js'
+		filename: 'js/bundle.js'
 	},
+	watch: true,
 	module: {
 		loaders: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loaders: ['babel-loader?presets[]=2015']
+				loaders: ['babel-loader?presets[]=es2015']
+			},
+			{
+				test: /\.jade$/,
+				exclude: /node_modules/,
+				loader: 'jade-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: "style-loader!css-loader"
+			},
+			{
+				test: /\.styl$/,
+				loader: "style-loader!css-loader!stylus-loader"
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot|svg)$/,
+				loader: 'file-loader'
 			}
 		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './templates/index.jade'
+		})
+	],
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: __dirname
 	}
 };
